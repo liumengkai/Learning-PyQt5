be039fd3d631e1f5b53dbd09d1091b20ad356a89
liumengkai <31892927+liumengkai@users.noreply.github.com>
2018-04-26 20:11:10 +0800

    Update test5.py

--- test5.py
+++ test5.py
@@ -1,14 +1,13 @@
 import sys
-from PyQt5.QtWidgets import QWidget,QPushButton,QApplication
+from PyQt5.QtWidgets import QApplication,QWidget,QToolTip,QPushButton
 from PyQt5.QtCore import QCoreApplication
-
 class Example(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
     def initUI(self):
-        qbtn= QPushButton('Quit',self)
-        qbtn.clicked.connect(QCoreApplication.instance().quit)
+        qbtn=QPushButton('Quit',self)           #创建一个按钮，第一个是按钮的文本，第二个是按钮的父类
+        qbtn.clicked.connect(QCoreApplication.instance().quit)    #这段代码让按钮quit和动作退出程序联系起来
         qbtn.resize(qbtn.sizeHint())
         qbtn.move(50,50)
         self.setGeometry(300,300,250,150)
